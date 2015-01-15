30b74c3a19a36299fcbf1253ee91293678f588d6
Graham Clapham <graham.clapham@thomsonreuters.com>
2015-01-15 17:06:12 +0000

    Updates to the GitHub controller.

--- app/js/modules/github/githubController.js
+++ app/js/modules/github/githubController.js
@@ -1,5 +1,12 @@
 githubModule.controller('GithubController', ['$scope', 'GitHubStreamData', 'GithubPublicApi', '$http', function($scope, GitHubStreamData, GithubPublicApi, $http) {
-    $scope.testData = "Test data bollox"
-    $scope.searccData = GithubPublicApi.search();
+    $scope.testData = "Test data bollox";
+    $scope.loaded = "NOT LOADED";
+
+    GithubPublicApi.search().then(function(resp){
+        console.log("Search done...",resp)
+        $scope.gitSearchData = resp;
+        $scope.loaded = "HAS LOADED"
+        $scope.items = resp.items
+    });
 }]);
 
