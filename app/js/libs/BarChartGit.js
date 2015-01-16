af52a95ee2c8229db14c321a542fd87f2978001b
Graham Clapham <graham.clapham@thomsonreuters.com>
2015-01-16 15:38:38 +0000

    Added D3 and got the SVG base rendering to stage on the chart.

--- app/js/libs/BarChartGit.js
+++ app/js/libs/BarChartGit.js
@@ -2,9 +2,14 @@ var BarChartGit = (function(target, opt_data, opt_config){
     var _scope = function(target, opt_data, opt_config){
         this._target = target;
         this._data = opt_data || [];
-
+        this.backgroundColor = "#353535"
         this.width      = 500;
         this.height     = 400;
+        this.padding = {t:40, r:40, b:40,l:40};
+        this._svg;
+        this._gridLinesHorizontal = null;
+        this._gridLinesVertical = null;
+
         this._scaleX    = null;
         this._scaleY     = null;
         /*
@@ -17,17 +22,43 @@ var BarChartGit = (function(target, opt_data, opt_config){
          score
          */
         this.xProperty = "stargazers_count";
-        _init()
+        _init.call(this);
 
     }
 //-- internal logic
 
     var _init = function(){
-
+        _init_svg.call(this);
     }
-    var _initSvg = function(){
 
+   var _init_svg =function(){
+        this._svg = d3.select( "#lineChartRender" )
+
+       this._svg
+            .style('width', this.width+ "px")
+            .style('height',  this.height+ "px")
+            .style('background-color',  this.backgroundColor)
+            .append('svg:svg')
+            .attr('width',this.width)
+            .attr('height',this.height )
+            .attr('class', "chartSvg")
+            .append('svg:g')
+            .attr('transform', "translate(" + this.padding.l + "," + this.padding.t  + ")");
+
+       console.log("this._svg ",this._svg );
+
+
+       this._gridLinesHorizontal  =
+            this._svg
+                .append('svg:g')
+                .attr('class', 'gridlinesH')
+
+        this._gridLinesVertical  =
+            this._svg
+                .append('svg:g')
+                .attr('class', 'gridlinesV');
     }
+
     var _onDataChanged = function(){
         console.log("Data Changed");
     }
--- app/js/modules/github/GithubController.js
+++ app/js/modules/github/GithubController.js
@@ -2,23 +2,30 @@ githubModule.controller('GithubController', ['$scope', 'GitHubStreamData', 'Gith
     $scope.testData = "Test data bollox";
     $scope.loaded = "NOT LOADED";
     $scope.searchTerm = "Three";
+    $scope.lineChartHolderId = "lineChartRender";
 //-- methods
     $scope.searchOn=function(value){
         $scope.searchTerm = value;
         $scope.items = [];
-        $scope.loaded = "Looking for "+$scope.searchTerm
+        $scope.loaded = "Looking for "+$scope.searchTerm;
         GithubPublicApi.search(value).then(function(resp){
-            console.log("Search done...",resp)
+            console.log("Search done...",resp);
             $scope.gitSearchData = resp;
-            $scope.loaded = "HAS LOADED"
-            $scope.items = resp.items
+            $scope.loaded = "HAS LOADED";
+            $scope.items = resp.items;
         });
 
     };
     //-- create the d3 chart
-    $scope.initD3Chart = function(){
+    $scope.initD3Chart = function(targ){
+        $scope.barChartGit = new BarChartGit(targ);
+        console.log("$scope.barChartGit ",$scope.barChartGit);
+        console.log("targ ------------ ",targ);
+    };
 
-    }
+    $scope.LineChartRenderElement = document.getElementById($scope.lineChartHolderId);
 
+    console.log("$scope.LineChartRenderElement  ",$scope.LineChartRenderElement)
+    $scope.initD3Chart($scope.LineChartRenderElement);
 }]);
 
